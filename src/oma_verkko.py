30c7d9cb8ccdaa47cf2bc326ae019ac43dff1949
Pontus <pontus.hedlund@gmail.com>
2022-08-12 00:29:11 +0300

    Koulutus säädetty 10 epookkiin. Tarkkuus 98,04%

--- src/oma_verkko.py
+++ src/oma_verkko.py
@@ -30,7 +30,7 @@ start = perf_counter()
 historia = neuroverkko.sovita(
     X_koulutus,
     y_koulutus,
-    epookit=25,
+    epookit=10,
     alijoukon_koko=100,
     validaatiodata=(X_testi, y_testi),
 )
